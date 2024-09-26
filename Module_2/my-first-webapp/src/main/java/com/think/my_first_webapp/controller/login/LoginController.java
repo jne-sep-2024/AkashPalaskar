611b09bd4e9575d0614035ba7440405f63b446f8
Akash-Thikitive <akash.palaskar@thinkitive.com>
2024-09-26 19:45:18 +0530

    feat:create login page,hard code authetication,start todo,add todo feature, delete feature , update feature

--- Module_2/my-first-webapp/src/main/java/com/think/my_first_webapp/controller/login/LoginController.java
+++ Module_2/my-first-webapp/src/main/java/com/think/my_first_webapp/controller/login/LoginController.java
@@ -1,21 +1,36 @@
 package com.think.my_first_webapp.controller.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("name")
+
 public class LoginController {
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
+@Autowired
+private AuthenticationService authenticationService;
+//    private Logger logger = LoggerFactory.getLogger(getClass());
 
-    @RequestMapping("/login")
-    public String goToLoginPage(@RequestParam String name, ModelMap model) {
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String goToLoginPage() {
+        return "login";
+    }
 
-        model.put("message", "welcome, " + name);
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
+        System.out.println(name);
+        System.out.println(password);
+        model.put("name",name);
+        if(authenticationService.authenticate(name,password)){
+            return "welcome";
+        }
+        model.put("error","Invalid Credentidal");
 
         return "login";
     }
---
+++ Module_2/my-first-webapp/src/main/resources/META-INF/resources/WEB-INF/jsp/todo.jsp
@@ -0,0 +1,32 @@
+<%@ taglib  prefix = "c" uri="http://java.sun.com/jsp/jstl/core"  %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="/webjars/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet" >
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Todo  page</title>
+</head>
+<body>
+<div class="container">
+<h1>Enter Todo Details</h1>
+    <form:form method="post" modelAttribute="todo">
+        <form:errors  path ="description" cssClass="text-warning"/>
+<br>
+       Description: <form:input type="text" name ="description"   path="description"  required="required" />
+
+       <form:input type="hidden" name ="description"   path="id"   />
+       <form:input type="hidden" name ="description"   path="done"   />
+      <input type="submit" class="btn btn-success"/>
+
+    </form:form>
+</div>
+
+<script src="/webjars/bootstrap/5.3.2/js/bootstrap.min.js"> </script>
+<script src="/webjars/jquery/3.7.0/jquery.js"> </script>
+
+</body>
+</html>
